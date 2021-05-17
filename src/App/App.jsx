15c438b3c7a0b63175074ae17f8c0720db5661bf
Abdulhamid Usman <Hazemidoo@gmail.com>
2021-05-17 08:09:10 +0100

    updated background image

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -9,6 +9,8 @@ import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
 
+import reception from '.../public/reception.jpg'
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -22,7 +24,7 @@ class App extends React.Component {
     render() {
         const { alert } = this.props;
         return (
-            <div className="vh-100 text-black"  style={{ backgroundImage: "url(/reception.jpg)" }}>
+            <div className="vh-100 text-black"  style={{ backgroundImage:`url(${reception})` }}>
                 <div className="container">
                     <div className="col-8 col-sm-offset-2 mx-auto">
                         {alert.message &&
